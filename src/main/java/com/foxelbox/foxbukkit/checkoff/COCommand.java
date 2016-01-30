1342e6e508a7f79ca9d72f091b10598ff5321954
Mark Dietzer <mriq91@gmail.com>
2016-01-30 20:39:12 +0100

    Constrain /co to world world

--- src/main/java/com/foxelbox/foxbukkit/checkoff/COCommand.java
+++ src/main/java/com/foxelbox/foxbukkit/checkoff/COCommand.java
@@ -101,7 +101,7 @@ public class COCommand implements CommandExecutor {
                         while (it.hasNext()) {
                             final UUID uuid = it.next();
                             final String name = checkoff.playerUUIDToName.get(uuid.toString());
-                            if (!checkoff.logBlock.isChangesListEmptyFor(ply, "player", name) || !checkoff.logBlock.isChangesListEmptyFor(ply, "player", name, "chestaccess")) {
+                            if (!checkoff.logBlock.isChangesListEmptyFor(ply, "player", name, "world", "world") || !checkoff.logBlock.isChangesListEmptyFor(ply, "player", name, "world", "world", "chestaccess")) {
                                 it.remove();
                             }
                         }
